31b320e611df90b663a31c2df535cb553e296ca3
PeterGrainfield <peter@vollegunord.work>
2018-02-13 21:48:09 +0900

    delete vscode

--- app.js
+++ app.js
@@ -1,18 +1,2 @@
-var Hello = /** @class */ (function () {
-    function Hello() {
-        this.count = 0;
-        console.log('Hello World');
-    }
-    Hello.prototype.tick = function () {
-        ++this.count;
-        console.log('count', this.count);
-    };
-    return Hello;
-}());
-var hello = new Hello();
-function main() {
-    hello.tick();
-    setTimeout(main, 1000);
-}
-main();
+console.log("Hello EcexuteAutomation");
 //# sourceMappingURL=app.js.map
